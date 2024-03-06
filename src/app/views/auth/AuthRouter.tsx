c4f4aef68817dbe067136339d1a60ab483df3763
laurarengifo09 <68392532+laurarengifo09@users.noreply.github.com>
2024-03-06 09:45:53 -0500

    Design register page

--- src/app/views/auth/AuthRouter.tsx
+++ src/app/views/auth/AuthRouter.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom"
 import { Login } from "./login/Login"
+import { Register } from "./register/Register"
 
 
 export function AuthRouter(){
     return(
         <Routes>
-            <Route path="/login" element={<Login />} />
+            <Route path="/login" element={< Login/>} />
+            <Route path="/register" element={<Register/>} />
             <Route path="*" element={<Navigate to="/login" replace />} />
 
 
